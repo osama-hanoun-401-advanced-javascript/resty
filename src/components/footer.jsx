97dbde0a268915a5ca0a4d2343766e3be0e09e79
Osama Hanoun <osamahanoun997@gmail.com>
2020-10-11 16:45:14 +0300

    finish lab

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import styles from '../components-styles/footer.scss';
+import React from 'react';
+
+const footer = () => {
+    return (
+        <footer className={styles}>
+            <p>© 2020 Osama Hanoun</p>
+        </footer>
+    )
+};
+
+export default footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import styles from '../components-styles/header.scss';
+import React from 'react';
+
+const header = () => {
+    return (
+        <header className={styles.header}>
+           <h1>RUSTy</h1>
+        </header>
+    )
+};
+
+export default header;
