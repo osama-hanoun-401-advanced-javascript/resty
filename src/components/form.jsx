ce23d0ec43248afd6e7c6ccf99ab60cdeed3f66d
Osama Hanoun <osamahanoun997@gmail.com>
2020-10-12 15:18:18 +0300

    fix form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -36,9 +36,3 @@ const form = (props) => {
 };
 
 export default form;
-
-{/* <div>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div> */}
