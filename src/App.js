fe93289bda5e1ca1a826fa4f41e26c69d14316ad
Osama Hanoun <osamahanoun997@gmail.com>
2020-10-13 21:42:29 +0300

    finish lab 27

--- src/App.js
+++ src/App.js
@@ -1,74 +1,36 @@
 import React from 'react';
 import './components-styles/reset.scss'
+import './components-styles/colors.scss'
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import Form from './components/form.jsx';
-
-// const Header = () => {
-//   return (
-//     <header>
-//       <h1>RESTy</h1>
-//     </header>
-//   )
-// };
-
-// const Footer = () => <footer>&copy 2020 401d4</footer>
-
-// //Main
-// class Main extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     // init state
-//     this.state = {
-//       words: 'Initial State'
-//     }
-//   }
-
-//   // methods inside of class
-//   handleClick = e => {
-//     let words = this.state.words.split('').reverse().join('');
-//     // update state.words
-//     this.setState({words}); // re-render 
-//   }
-
-//   handleInput = e => {
-//     // update state.words
-//     let words = e.target.value;
-//     this.setState({words}); // re-render 
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h3>{this.state.words}</h3>
-//         <input onChange={this.handleInput}/>
-//         <button onClick={this.handleClick}>Reverse</button>
-//       </div>
-//     )
-//   }
-// }
-
-
+import Results from './components/results.jsx'
+import { When } from 'react-if'
 class App extends React.Component {
   // init state
   state = {
-    method: '',
-    url: '',
+    count: null,
+    resultArr: null,
+    footerToggle: false,
   };
-  formHandler = (e) => {
-    e.preventDefault();
+  formHandler = (count, resultArr) => {
     this.setState({
-      method: e.target.method.value,
-      url: e.target.url.value,
+      count: count,
+      resultArr: resultArr,
+      footerToggle: !this.state.footerToggle,
     });
   }
+
+
   render() {
     return (
       <React.Fragment>
         <Header />
-        <Form submit={this.formHandler.bind(this)} method={this.state.method} url={this.state.url}
-        />
-        <Footer />
+        <Form formHandler={this.formHandler} />
+        <When condition={!!this.state.resultArr}>
+          <Results results={this.state.resultArr} />
+        </When>
+        <Footer footerClass={this.state.footerToggle} />
       </React.Fragment>
     )
   }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,38 +1,65 @@
 import '../components-styles/form.scss';
 import React from 'react';
+import superagent from 'superagent'
 
-const form = (props) => {
-    return (
-        <main>
-            <div>
-                <form onSubmit={props.submit} target="_blank">
-                    {/* <label htmlFor="url">URL</label> */}
-                    <input id="url" type="text" name="url" placeholder="url" required />
-                    <button type="submit"> GO! </button>
-                    <br />
-                    <input type="radio" id="get" name="method" value="get" required />
-                    <label for="get">GET</label>
-                    <input type="radio" id="post" name="method" value="post" required />
-                    <label for="post">POST</label>
-                    <input type="radio" id="put" name="method" value="put" required />
-                    <label for="put">PUT</label>
-                    <input type="radio" id="delete" name="method" value="delete" required />
-                    <label for="delete">DELETE</label>
-                </form>
-                <table>
-                    <tr>
-                        <td>
-                            {props.method}
-                        </td>
-                        <td>
-                            {props.url}
-                        </td>
-                    </tr>
-                </table>
+let _handleSubmit = async (e, formHandler) => {
+    e.preventDefault();
+    console.log(e.target.textArea.value);
+    let data;
+    try {
+        if (this.state.textArea) data = await superagent[e.target.method.value](e.target.url.value).send(e.target.textArea.value)
+        else data = await superagent[e.target.method.value](e.target.url.value);
+    } catch (error) {
+        data = 'Invalid Link '
+    }
 
-            </div>
-        </main>
-    )
-};
+    let count = '5';
 
-export default form;
+    formHandler(count, data, true)
+}
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            textArea: false
+        }
+    }
+    _handleTextArea = (e) => {
+        let clickedRadio = e.target.value;
+        if (clickedRadio) {
+
+            console.log('clickedRadio????????', clickedRadio)
+            if ((['get', 'delete']).includes(clickedRadio)) {
+                this.setState({ textArea: false });
+            }
+            else if ((['post', 'put']).includes(clickedRadio))
+                this.setState({ textArea: true });
+        }
+    }
+    render() {
+        return (
+            <main>
+                <div>
+                    <form onClick={this._handleTextArea} onSubmit={(e) => _handleSubmit(e, this.props.formHandler)}>
+                        <input id="url" type="url" name="url" placeholder="url" required />
+                        <button type="submit"> GO! </button>
+                        <br />
+                        <input type="radio" id="get" name="method" value="get" required />
+                        <label htmlFor="get">GET</label>
+                        <input type="radio" id="post" name="method" value="post" required />
+                        <label htmlFor="post">POST</label>
+                        <input type="radio" id="put" name="method" value="put" required />
+                        <label htmlFor="put">PUT</label>
+                        <input type="radio" id="delete" name="method" value="delete" required />
+                        <label htmlFor="delete">DELETE</label>
+                        {this.state.textArea ? < textarea name="textArea" id="body" cols="30" rows="10" /> : null}
+                    </form >
+                </div>
+            </main >
+
+        )
+    }
+}
+
+export default Form;
