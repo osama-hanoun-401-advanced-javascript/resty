8ce7d4c20bdaff27633bd35fc86566d35eb0b53a
OsamaHanoun <OsamaHanoun@users.noreply.github.com>
2020-10-17 13:17:37 +0000

    deploy: 5738f0ce2cc2345804bc5e3a6253faffaaec4279

---
+++ precache-manifest.9ed5867fb3edd28821e54eacc5aa3cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8137891462238d19a4993c37c62efd",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "cb2e1c529f042e2baabb",
+    "url": "/resty/static/css/main.3ea746a9.chunk.css"
+  },
+  {
+    "revision": "6b5e163411f17201dc5c",
+    "url": "/resty/static/js/2.2ac42359.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resty/static/js/2.2ac42359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2e1c529f042e2baabb",
+    "url": "/resty/static/js/main.18d4cf34.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
