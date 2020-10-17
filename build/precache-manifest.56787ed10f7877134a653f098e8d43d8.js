56d65c3a9c3ec5199713f61e8a69b55d482ddc60
Osama Hanoun <osamahanoun997@gmail.com>
2020-10-17 16:26:36 +0300

    build

---
+++ build/precache-manifest.56787ed10f7877134a653f098e8d43d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f14198fa0c858eeba732bbec7fcf5b1",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "161b2d032367d7e1de70",
+    "url": "/resty/static/css/main.f93f01a0.chunk.css"
+  },
+  {
+    "revision": "6b5e163411f17201dc5c",
+    "url": "/resty/static/js/2.2ac42359.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resty/static/js/2.2ac42359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161b2d032367d7e1de70",
+    "url": "/resty/static/js/main.80a679bd.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
